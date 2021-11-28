e0493929bb761e619139e9a33132f7968c2789f2
CloudFoundry276 <cloudfoundry276@gmail.com>
2021-11-28 19:45:08 +0530

    staged changes

--- helloworld.php
+++ helloworld.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Hello World</title>
+	<title>Home</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 	<style type="text/css">
